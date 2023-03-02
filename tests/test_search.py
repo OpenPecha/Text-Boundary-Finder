bd575e902d6a4e4e7b9667d6f5e51c48fab1ce3f
kaldan007 <kaldantenzin@gmail.com>
2023-03-02 14:43:08 +0530

    rename module and project name

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,11 +1,10 @@
-from src import search,search_v2
+from text_boundary_finder.search import search
 from pathlib import Path
-from . import PROJECT_PATH
 
 
 def test_search():
-    search_elem = Path("./data/elem.txt").read_text(encoding="utf-8")
-    pool_text = Path("./data/pool.txt").read_text(encoding="utf-8")
-    expected_result = Path("./data/expected_result.txt").read_text(encoding="utf-8")
+    search_elem = Path("./tests/data/elem.txt").read_text(encoding="utf-8")
+    pool_text = Path("./tests/data/pool.txt").read_text(encoding="utf-8")
+    expected_result = Path("./tests/data/expected_result.txt").read_text(encoding="utf-8")
     start,end = search(search_elem,pool_text)
     assert expected_result==pool_text[start:end]
