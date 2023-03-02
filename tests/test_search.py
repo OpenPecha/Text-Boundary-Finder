e7cb84797ba36e4618ef4585cc202a8931e5db6e
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-02 11:03:18 +0530

    added dependencies

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,11 +1,11 @@
-from search import search
+from src import search,search_v2
 from pathlib import Path
 from . import PROJECT_PATH
 
+
 def test_search():
-    
-    search_elem = Path("./test/data/elem.txt").read_text(encoding="utf-8")
-    pool_text = Path("./test/data/pool.txt").read_text(encoding="utf-8")
-    expected_result = Path("./test/data/expected_result.txt").read_text(encoding="utf-8")
+    search_elem = Path("./data/elem.txt").read_text(encoding="utf-8")
+    pool_text = Path("./data/pool.txt").read_text(encoding="utf-8")
+    expected_result = Path("./data/expected_result.txt").read_text(encoding="utf-8")
     start,end = search(search_elem,pool_text)
     assert expected_result==pool_text[start:end]
