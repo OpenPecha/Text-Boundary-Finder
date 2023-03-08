8cdcd9f6b815003fc09545b28078f873b90f44e9
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-08 14:57:26 +0530

    added valid order checker

--- text_boundary_finder/search.py
+++ text_boundary_finder/search.py
@@ -34,49 +34,70 @@ def closest_value(input_list, input_value,matches_comb):
 def get_closest_match(matches_comb,len_pool_text):
     dists = []
     for match_comb in matches_comb:
-        first_match,_,last_match = match_comb
+        first_match,mid_match,last_match = match_comb
+        valid_order = check_valid_order(match_comb)
+        if not valid_order:
+            dists.append(0)
+            continue
         cur_dist = last_match.end - first_match.start
         dists.append(cur_dist)
     i = closest_value(dists,len_pool_text,matches_comb)
     return matches_comb[i]
 
 def get_total_len_of_list_elems(elems):
-    len = 0
+    total_len= 0
     for elem in elems:
-        len+=len(elem)
-    return len
+        total_len+=len(elem)
+    return total_len
 
-def get_boundary_matches(elems_matches,elems,pos):
-    ret_elem = None
+def get_nearest_match(matches_comb,len_of_total_elems):
+    nearest_matches = []
+    standard_threshold = int(len_of_total_elems/3*0.3)
+    for match_comb in matches_comb:
+        first_match,mid_match,last_match = match_comb
+        valid_order = check_valid_order(match_comb)
+        if not valid_order:
+            continue
+        if (mid_match.start-first_match.end) < standard_threshold and (last_match.start-mid_match.end) < standard_threshold:
+            nearest_matches.append(match_comb)
+
+    return nearest_matches        
+
+
+def check_valid_order(match_comb):
+    first_match,mid_match,last_match = match_comb
+    if first_match.end <= mid_match.start and mid_match.end <= last_match.start:
+        return True
+    return False
+
+def get_boundary_matches(elems,pos):
+    ret_elem = []
+    elems_matches = fuzzy_searches(elems,pool_text)
     len_of_total_elems = get_total_len_of_list_elems(elems)
     matches_comb = []
     first_seg_matches,second_seg_matches,third_seg_matches = elems_matches
     for first_seg_match in first_seg_matches:
         for second_seg_match in second_seg_matches:
             for third_seg_match in third_seg_matches:
-                matches_comb.append((first_seg_match,second_seg_match,third_seg_match))
+                matches_comb.append([first_seg_match,second_seg_match,third_seg_match])
 
-    closest_match = get_closest_match(matches_comb,len_of_total_elems)
-    if pos == "first":
-        ret_elem = closest_match[0]
-    elif pos == "last":
-        ret_elem = closest_match[-1]
-    else:
-        ret_elem = closest_match[1]
+    closest_matches = get_nearest_match(matches_comb,len_of_total_elems)
+    for closest_match in closest_matches:
+        if pos == "first":
+            ret_elem.append(closest_match[0])
+        elif pos == "last":
+            ret_elem.append(closest_match[-1])
+        else:
+            ret_elem.append(closest_match[1])
     return ret_elem
 
 
 def search(searched_elem,pool_text):
     matches_comb = []
     first_elems,mid_elems,last_elems = get_elements(searched_elem)
-    first_elems_matches = fuzzy_searches(first_elems,pool_text)
-    mid_elems_matches = fuzzy_searches(mid_elems,pool_text)
-    last_elems_matches = fuzzy_searches(last_elems,pool_text)
-
-    first_matches = get_boundary_matches(first_elems_matches,first_elems,pos="first")
-    mid_matches = get_boundary_matches(mid_elems_matches,mid_elems,pos="mid")
-    last_matches = get_boundary_matches(last_elems_matches,last_elems,pos="last")
-
+    first_matches = get_boundary_matches(first_elems,pos="first")
+    mid_matches = get_boundary_matches(mid_elems,pos="mid")
+    last_matches = get_boundary_matches(last_elems,pos="last")
 
     for first_match in first_matches:
         for mid_match in mid_matches:
@@ -94,4 +115,5 @@ if __name__ == "__main__":
     pwd = os.getcwd()
     search_text = Path("./tests/data/elem.txt").read_text(encoding="utf-8")
     pool_text = Path("./tests/data/pool.txt").read_text(encoding="utf-8")
-    segs = search(search_text,pool_text)
+    start,end = search(search_text,pool_text)
+    Path("./tests/data/result.txt").write_text(pool_text[start:end])
